da128b948d54ff50151e5b069b06cde11a6ccd26
franciscoValente185 <franciscomaria.valente@gmail.com>
2026-09-14 13:29:57 +0000

    AP01(2): Ex em aula

---
+++ exercises/aula-pratica-bd-01-2.sql
@@ -0,0 +1,15 @@
+SELECT TOP 10 
+employee_id,
+first_name,
+last_name
+FROM HR.EMPLOYEES;
+
+SELECT TOP 10
+first_name,
+last_name,
+email,
+phone_number
+FROM HR.EMPLOYEES;
+
+SELECT TOP 10 *
+FROM HR.EMPLOYEES;
